8233c8d9568a0bac9f348fb3c45c8de39e6eff42
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-02-11 17:45:09 +0000

    Move alert to seperate section

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Add text to alert message, then continue building Section B
+// For Next Time: Complete alert message, then continue building Section B
 
 app.get('/', (req, res) => {
   res.render('home');
