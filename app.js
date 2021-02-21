6743d8503802fe91bc867714f0ba25aa083828cf
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-02-21 17:35:01 +0000

    Finish Home Page Section B

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Scale down 2nd image on small-screen + change font-size & text colour!
+// For Next Time: Get icons to add beneath Section C text
 
 app.get('/', (req, res) => {
   res.render('home');
