8470b1294a9e8647acc7c09a9030abbd1bb5c6b8
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-03-15 10:17:35 +0000

    Finish Home Page Section D

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Start adding text to Home Page Section D Image 4!
+// For Next Time: Start building Section E of Home Page!
 
 app.get('/', (req, res) => {
   res.render('home');
