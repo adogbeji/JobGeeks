556f65738d74482d8c82c904ba453e9cdb1b1eda
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-04-15 11:13:25 +0100

    Finish Home Page Section H

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Reposition text in sequence section on small-screen!
+// For Next Time: Start building Home Page Section I!
 
 app.get('/', (req, res) => {
   res.render('home');
