3e3d3659584a7728ed4fe168aa35152af8ab3a55
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-05-15 11:34:43 +0100

    Start adding code for Home Page Section L

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Start building Home Page Section L!
+// For Next Time: Continue building Home Page Section L!
 
 app.get('/', (req, res) => {
   res.render('home');
