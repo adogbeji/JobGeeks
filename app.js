333ceb5bb5e206740661a6705a82057bcfdd880b
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-05-29 10:55:48 +0100

    Start building Column 4 of Home Page Footer section

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 const User = require('./models/user-model');
 
-// For Next Time: Build Column 4 of Home Page Footer Section
+// For Next Time: Continue building Column 4 of Home Page Footer Section
 
 app.get('/', (req, res) => {
   res.render('home');
